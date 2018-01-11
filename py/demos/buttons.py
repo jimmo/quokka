ef9f92f50add294d050b623c5eb5ab366ba48b52
Jim Mussared <jim.mussared@gmail.com>
2018-01-11 21:33:58 +1100

    add update script for windows

--- py/demos/buttons.py
+++ py/demos/buttons.py
@@ -1,15 +1,25 @@
 import quokka
 
+quokka.display.fill(0)
+quokka.display.text(quokka.radio.version(), 0, 0, 1)
+quokka.display.show()
+quokka.sleep(1000)
+
 while True:
   quokka.display.fill(0)
+  quokka.neopixels.clear()
   if quokka.buttons.a.is_pressed():
     quokka.display.text('a', 5, 5, 1)
+    quokka.neopixels.set_pixel(0, 255, 0, 0)
   if quokka.buttons.b.is_pressed():
     quokka.display.text('b', 20, 5, 1)
+    quokka.neopixels.set_pixel(1, 255, 0, 0)
   if quokka.buttons.c.is_pressed():
     quokka.display.text('c', 35, 5, 1)
+    quokka.neopixels.set_pixel(2, 255, 0, 0)
   if quokka.buttons.d.is_pressed():
     quokka.display.text('d', 50, 5, 1)
+    quokka.neopixels.set_pixel(3, 255, 0, 0)
   if quokka.buttons.usr.is_pressed():
     quokka.display.text('u', 65, 5, 1)
   quokka.display.show()
---
+++ py/update.bat
@@ -0,0 +1,11 @@
+rem -- download 'sync' from SysInternals -- short link http://goo.gl/32dbAJ
+
+rem -- first time only - copy support files
+rem copy quokka.py D:\
+rem copy boot.py D:\
+rem mkdir D:\drivers
+rem copy drivers\*.py D:\drivers
+
+rem -- update main.py
+copy main.py D:\main.py
+sync D:\
