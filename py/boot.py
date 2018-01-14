503736c7e238e362138d2571d7d6a8936d1e82a2
hogesonline <hogesonline@gmail.com>
2018-01-14 12:41:00 +1100

    clear neopixel bounce
    Plugging in USB often causes neopixels to turn on, these lines in boot.py turn them off on soft reboot.

--- py/boot.py
+++ py/boot.py
@@ -3,6 +3,8 @@
 
 import machine
 import pyb
+import quokka
+quokka.neopixels.show()
 #pyb.main('main.py') # main script to run after this one
 #pyb.usb_mode('VCP+MSC') # act as a serial and a storage device
 #pyb.usb_mode('VCP+HID') # act as a serial device and a mouse
